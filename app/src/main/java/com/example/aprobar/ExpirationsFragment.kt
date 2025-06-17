9ec5839dce9da2eb45ce561a90596f7a25321b7c
Agustin Braco <bracoagustin@gmail.com>
2025-06-17 20:20:08 -0300

    [Main] - Fragments refactor

--- app/src/main/java/com/example/aprobar/ExpirationsFragment.kt
+++ app/src/main/java/com/example/aprobar/ExpirationsFragment.kt
@@ -1,5 +1,17 @@
 package com.example.aprobar
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class ExpirationsFragment : Fragment(R.layout.fragment_expirations) {}
+class ExpirationsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) : View? {
+        return inflater.inflate(R.layout.fragment_expirations, container, false)
+    }
+}
